65b9988894aa9c35c9ef2ae35255dfb233d1f740
AndyHan <hanxu@AndyHans-Air.attlocal.net>
2024-09-10 22:02:34 -0400

    added changes

--- Try/ProofML/go.mod
+++ Try/ProofML/go.mod
@@ -1,6 +1,8 @@
 module sudokuChecker
 
-go 1.20
+go 1.21
+
+toolchain go1.23.0
 
 require (
 	github.com/consensys/gnark v0.10.0
@@ -14,7 +16,8 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
 	github.com/google/pprof v0.0.0-20230817174616-7a8ec2ada47b // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/ingonyama-zk/icicle v0.0.0-20230928131117-97f0079e5c71 // indirect
+	github.com/ingonyama-zk/iciclegnark v0.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
@@ -22,7 +25,6 @@ require (
 	github.com/rs/zerolog v1.30.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
--- Try/ProofML/main.go
+++ Try/ProofML/main.go
@@ -1,120 +1,183 @@
-package main
+// package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"math/big"
+// 	"os"
 
-	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/math/cmp"
-)
+// 	"github.com/consensys/gnark-crypto/ecc"
+// 	"github.com/consensys/gnark/backend/groth16"
+// 	"github.com/consensys/gnark/frontend"
+// 	"github.com/consensys/gnark/frontend/cs/r1cs"
+// 	"github.com/consensys/gnark/std/math/cmp"
+// )
 
-// ProveModelCircuit defines the circuit structure
-type ProveModelCircuit struct {
-	A        [][][]frontend.Variable `gnark:",secret"` // Weights (m x n)
-	Biases   [][]frontend.Variable    `gnark:",secret"` // Biases (m x 1)
-	X        []frontend.Variable      `gnark:",public"` // Input vector (n)
-	Expected []frontend.Variable      `gnark:",public"` // Expected outputs (slice)
-}
+// // ProveModelCircuit defines the circuit structure for a two-layer neural network with 3D weights, 2D biases, and 2D inputs
+// type ProveModelCircuit struct {
+// 	Weights  [2][3][4]frontend.Variable `gnark:",secret"` // 2 layers, each with 3 neurons, each neuron has 4 inputs
+// 	Biases   [2][3]frontend.Variable    `gnark:",secret"` // 2 layers, each with 3 biases (one for each neuron)
+// 	X        [3][4]frontend.Variable    `gnark:",public"` // 2D array for inputs (3 sets of inputs, each with 4 values)
+// 	Expected [3]frontend.Variable       `gnark:",public"` // Expected output (3 values from final layer)
+// }
 
-// Define defines the circuit's constraints
-func (circuit *ProveModelCircuit) Define(api frontend.API) error {
-	// Number of neurons
-	numberNeurons := len(circuit.A[0])
+// // Define the constraints of the circuit
+// func (circuit *ProveModelCircuit) Define(api frontend.API) error {
+// 	// First layer output (3 neurons)
+// 	outputLayer1 := make([]frontend.Variable, 3)
 
-	// Initialize the output variable
-	output := make([]frontend.Variable, numberNeurons)
+// 	// Calculate the output for each neuron in the first layer using the first set of inputs
+// 	for i := 0; i < 3; i++ {
+// 		outputLayer1[i] = circuit.Biases[0][i] // First layer biases
+// 		for j := 0; j < 4; j++ {
+// 			outputLayer1[i] = api.Add(outputLayer1[i], api.Mul(circuit.Weights[0][i][j], circuit.X[i][j]))
+// 		}
+// 		// Apply ReLU activation
+// 		isNegative := cmp.IsLess(api, outputLayer1[i], 0)
+// 		outputLayer1[i] = api.Select(isNegative, 0, outputLayer1[i])
+// 	}
 
-	// Calculate the output for each neuron
-	for i := 0; i < numberNeurons; i++ {
-		output[i] = circuit.Biases[0][i]
-		for j := 0; j < len(circuit.X); j++ {
-			output[i] = api.Add(output[i], api.Mul(circuit.A[0][i][j], circuit.X[j]))
-		}
-		// Apply ReLU activation
-		output[i] = api.Select(cmp.IsLess(api, output[i], 0), 0, output[i])
-	}
+// 	// Second layer output (final layer with 3 neurons)
+// 	outputLayer2 := make([]frontend.Variable, 3)
 
-	// Find the maximum value (argmax) in the output layer
-	maxVal := output[0]
-	maxIdx := frontend.Variable(0)
-	for i := 1; i < numberNeurons; i++ {
-		isLess := cmp.IsLess(api, maxVal, output[i])
-		maxVal = api.Select(isLess, output[i], maxVal)
-		maxIdx = api.Select(isLess, i, maxIdx)
-	}
+// 	// Calculate the output for each neuron in the second layer
+// 	for i := 0; i < 3; i++ {
+// 		outputLayer2[i] = circuit.Biases[1][i] // Second layer biases
+// 		for j := 0; j < 3; j++ {
+// 			outputLayer2[i] = api.Add(outputLayer2[i], api.Mul(circuit.Weights[1][i][j], outputLayer1[j]))
+// 		}
+// 		// Apply ReLU activation
+// 		isNegative := cmp.IsLess(api, outputLayer2[i], 0)
+// 		outputLayer2[i] = api.Select(isNegative, 0, outputLayer2[i])
+// 	}
 
-	// Assert that the predicted class matches the expected output
-	api.AssertIsEqual(maxIdx, circuit.Expected)
+// 	// Assert that the final layer output matches the expected output
+// 	for i := 0; i < 3; i++ {
+// 		fmt.Print("expetcted", outputLayer2[i])
+// 		api.AssertIsEqual(outputLayer2[i], circuit.Expected[i])
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func readJSONFile(filename string, target interface{}) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return fmt.Errorf("error opening file %s: %v", filename, err)
-	}
-	defer file.Close()
+// // float64ToBigInt converts a float64 to a big.Int by scaling
+// func float64ToBigInt(value float64) *big.Int {
+// 	scaledValue := value * 1e9 // Scaling factor to convert float to int
+// 	return big.NewInt(int64(scaledValue))
+// }
 
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		return fmt.Errorf("error reading file %s: %v", filename, err)
-	}
+// func float64ToBigIntNoScale(value float64) *big.Int {
+// 	return big.NewInt(int64(value)) // No scaling applied here
+// }
 
-	if err := json.Unmarshal(bytes, target); err != nil {
-		return fmt.Errorf("error unmarshaling JSON from file %s: %v", filename, err)
-	}
+// func main() {
+// 	// Step 1: Open the JSON files
+// 	weightsFile, err := os.Open("weights.json")
+// 	if err != nil {
+// 		fmt.Println("Error opening weights file:", err)
+// 		return
+// 	}
+// 	defer weightsFile.Close()
 
-	return nil
-}
+// 	inputsFile, err := os.Open("inputs.json")
+// 	if err != nil {
+// 		fmt.Println("Error opening inputs file:", err)
+// 		return
+// 	}
+// 	defer inputsFile.Close()
 
-func main() {
-	var weightsData struct {
-		Weights [][][]float64 `json:"weights"`
-		Biases [][]float64 `json:"biases"`
-	}
-	
-	var expectedData struct {
-		Expected []float64 `json:"outputs"`
-	}
+// 	outputsFile, err := os.Open("outputs.json")
+// 	if err != nil {
+// 		fmt.Println("Error opening outputs file:", err)
+// 		return
+// 	}
+// 	defer outputsFile.Close()
 
-	// Read JSON files
-	if err := readJSONFile("weights.json", &weightsData); err != nil {
-		fmt.Println(err)
-		return
-	}
+// 	// Step 2: Read the file contents into byte slices
+// 	weightsByteValue, err := ioutil.ReadAll(weightsFile)
+// 	if err != nil {
+// 		fmt.Println("Error reading weights file:", err)
+// 		return
+// 	}
 
-	if err := readJSONFile("outputs.json", &expectedData); err != nil{
-		fmt.Println(err)
-		return
-	}
-	
+// 	inputsByteValue, err := ioutil.ReadAll(inputsFile)
+// 	if err != nil {
+// 		fmt.Println("Error reading inputs file:", err)
+// 		return
+// 	}
 
-	// Convert JSON data to frontend.Variable
-	A := make([][][]frontend.Variable, len(weightsData.Weights))
-	for i, layer := range weightsData.Weights {
-		A[i] = make([][]frontend.Variable, len(layer))
-		for j, neuron := range layer {
-			A[i][j] = make([]frontend.Variable, len(neuron))
-			for k, weight := range neuron {
-				A[i][j][k] = frontend.Variable(weight)
-			}
-		}
-	}
-	
-	Biases := make([][]frontend.Variable, len(weightsData.Biases))
-	for i, biasLayer := range weightsData.Biases {
-		Biases[i] = make([]frontend.Variable, len(biasLayer))
-		for j, bias := range biasLayer {
-			Biases[i][j] = frontend.Variable(bias)
-		}
-	}
-	
-	Output := make([]frontend.Variable, len(expectedData.Expected))
-	for i, datads := range expectedData.Expected {
-		Output[i] = frontend.Variable(datads)
-	}
-	fmt.Print(Output)
-}
+// 	outputsByteValue, err := ioutil.ReadAll(outputsFile)
+// 	if err != nil {
+// 		fmt.Println("Error reading outputs file:", err)
+// 		return
+// 	}
+
+// 	// Step 3: Unmarshal the byte slices into the respective structs
+// 	var weightsData struct {
+// 		Weights [2][3][4]float64 `json:"weights"` // 3D array for weights
+// 		Biases  [2][3]float64    `json:"biases"`  // 2D array for biases
+// 	}
+
+// 	var inputData struct {
+// 		Inputs [3][4]float64 `json:"inputs"` // 2D array for inputs (3 sets of 4 values)
+// 	}
+
+// 	var expectedData struct {
+// 		Expected [3]float64 `json:"outputs"`
+// 	}
+
+// 	err = json.Unmarshal(weightsByteValue, &weightsData)
+// 	if err != nil {
+// 		fmt.Println("Error unmarshalling weights JSON:", err)
+// 		return
+// 	}
+
+// 	err = json.Unmarshal(inputsByteValue, &inputData)
+// 	if err != nil {
+// 		fmt.Println("Error unmarshalling inputs JSON:", err)
+// 		return
+// 	}
+
+// 	err = json.Unmarshal(outputsByteValue, &expectedData)
+// 	if err != nil {
+// 		fmt.Println("Error unmarshalling outputs JSON:", err)
+// 		return
+// 	}
+
+// 	// Create the circuit assignment
+// 	assignment := &ProveModelCircuit{}
+// 	// Assign weights and biases
+// 	for layer := 0; layer < 2; layer++ {
+// 		for i := 0; i < 3; i++ {
+// 			for j := 0; j < 4; j++ {
+// 				assignment.Weights[layer][i][j] = frontend.Variable(float64ToBigInt(weightsData.Weights[layer][i][j]))
+// 			}
+// 			assignment.Biases[layer][i] = frontend.Variable(float64ToBigInt(weightsData.Biases[layer][i]))
+// 		}
+// 	}
+
+// 	// Assign inputs and expected output
+// 	for i := 0; i < 3; i++ {
+// 		for j := 0; j < 4; j++ {
+// 			assignment.X[i][j] = frontend.Variable(float64ToBigInt(inputData.Inputs[i][j]))
+// 		}
+// 	}
+// 	for i := 0; i < 3; i++ {
+// 		assignment.Expected[i] = frontend.Variable(float64ToBigIntNoScale(expectedData.Expected[i]))
+// 	}
+
+// 	fmt.Println("expected assignment:", assignment.Expected)
+// 	var myCircuit ProveModelCircuit
+// 	witness, _ := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
+// 	cs, _ := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &myCircuit)
+// 	pk, vk, _ := groth16.Setup(cs)
+// 	proof, errproof := groth16.Prove(cs, pk, witness)
+// 	fmt.Println("Error Proving: ", errproof)
+// 	publicWitness, _ := witness.Public()
+// 	errverify := groth16.Verify(proof, vk, publicWitness)
+// 	fmt.Println("Error in Verifying: ", errverify)
+// 	if errverify == nil && errproof == nil {
+// 		fmt.Println("Verification succeeded")
+// 	}
+// }
